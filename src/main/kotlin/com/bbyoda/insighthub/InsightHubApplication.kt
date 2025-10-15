178be8e81311a5efed1079ba50a173e97e1f8556
Tri Dung Ngo <tri.dung.ngo@accenture.com>
2025-10-16 00:32:34 +0200

    init project

---
+++ src/main/kotlin/com/bbyoda/insighthub/InsightHubApplication.kt
@@ -0,0 +1,11 @@
+package com.bbyoda.insighthub
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InsightHubApplication
+
+fun main(args: Array<String>) {
+	runApplication<InsightHubApplication>(*args)
+}
