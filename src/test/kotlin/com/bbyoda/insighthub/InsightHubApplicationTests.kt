8d05a17434d34703010697f7dd49222cc17eec64
Tri Dung Ngo <tridungngo.io@gmail.com>
2025-10-16 00:32:34 +0200

    init project

---
+++ src/test/kotlin/com/bbyoda/insighthub/InsightHubApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bbyoda.insighthub
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InsightHubApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
