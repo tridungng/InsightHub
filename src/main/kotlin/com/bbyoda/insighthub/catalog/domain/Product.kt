eeda9ae9e88769143138728927c2597fe26332ac
Tri Dung Ngo <tri.dung.ngo@accenture.com>
2025-10-19 00:57:55 +0200

    feat(catalog): update domain models and add events

--- src/main/kotlin/com/bbyoda/insighthub/catalog/domain/Product.kt
+++ src/main/kotlin/com/bbyoda/insighthub/catalog/domain/Product.kt
@@ -1,14 +1,20 @@
 package com.bbyoda.insighthub.catalog.domain
 
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.Id
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
 import java.math.BigDecimal
 import java.util.UUID
 
-
-class Product (
-    val id: UUID = UUID.randomUUID(),
+@Entity
+@Table(name = "products")
+class Product(
+    @Id val id: UUID = UUID.randomUUID(),
     var name: String,
     var description: String,
     var price: BigDecimal,
     var stock: Int,
-    var category: Category
-    )
+    @ManyToOne(fetch = FetchType.LAZY) var category: Category?
+)
