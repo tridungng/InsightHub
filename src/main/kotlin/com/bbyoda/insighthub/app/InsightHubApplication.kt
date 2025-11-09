eeb4915f0b6775f5a4620370d310f52c88ebd916
bbyoda <tridungngo.io@gmail.com>
2025-11-09 17:00:55 +0100

    refactor: move main method to app folder

--- src/main/kotlin/com/bbyoda/insighthub/app/InsightHubApplication.kt
+++ src/main/kotlin/com/bbyoda/insighthub/app/InsightHubApplication.kt
@@ -1,4 +1,4 @@
-package com.bbyoda.insighthub
+package com.bbyoda.insighthub.app
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 class InsightHubApplication
 
 fun main(args: Array<String>) {
-	runApplication<InsightHubApplication>(*args)
+    runApplication<InsightHubApplication>(*args)
 }
