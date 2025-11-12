148d3fbdb4cbf4aee183fed482676498b1f860e4
bbyoda <tridungngo.io@gmail.com>
2025-11-12 21:24:43 +0100

    feat: add password changed domain event

---
+++ src/main/kotlin/com/bbyoda/insighthub/domains/security/domain/event/PasswordChanged.kt
@@ -0,0 +1,10 @@
+package com.bbyoda.insighthub.domains.security.domain.event
+
+import com.bbyoda.insighthub.shared.kernel.DomainEvent
+import com.bbyoda.insighthub.shared.types.UserId
+import java.time.Instant
+
+data class PasswordChanged(
+    val userId: UserId,
+    override val occurredAt: Instant = Instant.now()
+) : DomainEvent
