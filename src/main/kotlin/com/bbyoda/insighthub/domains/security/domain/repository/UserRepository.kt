6ac038b9736bf7fab03b9d6f46c5045ed744a1a9
bbyoda <tridungngo.io@gmail.com>
2025-11-12 21:44:22 +0100

    feat: impl register user use case

--- src/main/kotlin/com/bbyoda/insighthub/domains/security/domain/repository/UserRepository.kt
+++ src/main/kotlin/com/bbyoda/insighthub/domains/security/domain/repository/UserRepository.kt
@@ -5,7 +5,7 @@ import com.bbyoda.insighthub.shared.types.Email
 import com.bbyoda.insighthub.shared.types.UserId
 
 interface UserRepository {
-    fun save(user: User): User?
+    fun save(user: User): User
     fun findById(id: UserId): User?
     fun findByEmail(email: Email): User?
     fun existsByEmail(email: Email): Boolean
