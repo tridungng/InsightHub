0ad9e04da7ff676a366533d694c639e777c4f688
bbyoda <tridungngo.io@gmail.com>
2025-12-03 17:31:19 +0100

    feat: implement Kafka event publisher for order

---
+++ src/main/kotlin/com/bbyoda/insighthub/order/infrastructure/event/KafkaEventPublisher.kt
@@ -0,0 +1,21 @@
+package com.bbyoda.insighthub.order.infrastructure.event
+
+import com.bbyoda.insighthub.order.application.port.EventPublisher
+import com.bbyoda.insighthub.shared.kernel.DomainEvent
+import org.slf4j.LoggerFactory
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.stereotype.Component
+
+@Component
+class KafkaEventPublisher(
+    private val kafkaTemplate: KafkaTemplate<String, Any>
+) : EventPublisher {
+    private val log = LoggerFactory.getLogger(javaClass)
+    override fun publish(events: Collection<DomainEvent>) {
+        events.forEach { ev ->
+            val topic = "order.${ev::class.simpleName}"
+            kafkaTemplate.send(topic, ev)
+            log.debug("Published event {} to {}", ev, topic)
+        }
+    }
+}
