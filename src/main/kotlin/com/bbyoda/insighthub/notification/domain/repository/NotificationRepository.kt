c0add5fa40c8334bbd8a8d02c7af8868f2272049
bbyoda <tridungngo.io@gmail.com>
2026-01-03 11:57:05 +0100

    feat: update save method in NotificationRepository to return Notification

--- src/main/kotlin/com/bbyoda/insighthub/notification/domain/repository/NotificationRepository.kt
+++ src/main/kotlin/com/bbyoda/insighthub/notification/domain/repository/NotificationRepository.kt
@@ -3,7 +3,7 @@ package com.bbyoda.insighthub.notification.domain.repository
 import com.bbyoda.insighthub.notification.domain.model.Notification
 
 interface NotificationRepository {
-    fun save(notification: Notification)
+    fun save(notification: Notification): Notification
     fun findById(id: String): Notification?
     fun findPending(limit: Int = 100): List<Notification>
     fun findByUserId(userId: String, limit: Int = 50): List<Notification>
